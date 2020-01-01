31a306f02539e903893bd7d8bb4dad32178ac616
Conner Anderson <canderson@catalyte.io>
2019-12-31 17:32:01 -0700

    npm tweak

--- models/npc.model.js
+++ models/npc.model.js
@@ -3,25 +3,27 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const npcSchema = new Schema({
-    campaignId: {type: [String], required: true, default: 'TBD'},
-    image: {type: String },
-    name: {type: String, required: true},
-    sex: {type: String, required: true},
-    race: {type: String, required: true},
-    age: {type: Number, required: true},
-    height: {type: Number, required: true},
-    alignment: {type: String, required: true },
-    profession: {type: [String], required: true},
+    campaignId: {type: [String], required: true, default: ['TBD']},
+    image: {type: String, default: 'TBD'},
+    name: {type: String, required: true, default: 'TBD'},
+    sex: {type: String, required: true, default: 'TBD'},
+    race: {type: String, required: true, default: 'TBD'},
+    age: {type: Number, required: true, default: 0},
+    height: {type: Number, required: true, default: 0},
+    eyes: {type: String, required: true, default: 'TBD'},
+    hair: {type: String, required: true, default: 'TBD'},
+    alignment: {type: String, required: true, default: 'TBD'},
+    profession: {type: [String], required: true, default: ['TBD']},
     affiliations: {
-        organizations: {type: [String], required: true},
-        people: {type: [String], required: true}
+        organizations: {type: [String], required: true, default: ['TBD']},
+        people: {type: [String], required: true, default: ['TBD']}
     },
-    goals: {type: [String], required: true},
-    ticks: {type: [String]},
-    flaws: {type: [String]},
-    oddities: {type: [String], required: true},
-    description: {type: [String], required: true},
-    notes: {type: [String]}
+    goals: {type: [String], required: true, default: ['TBD']},
+    ticks: {type: [String], default: ['TBD']},
+    flaws: {type: [String], default: ['TBD']},
+    oddities: {type: [String], required: true, default: ['TBD']},
+    description: {type: [String], required: true, default: ['TBD']},
+    notes: {type: [String], default: ['TBD']}
 })
 //Things to possibly add TODO:
 //      Link to full monster block if they have spells and 
